b8d77a5051900c9444a2e6671152ddd10f278312
vitormatheus-18 <vitormmmorais@fac.pe.senac.br>
2021-06-23 00:08:56 -0300

    Create buttons in App.tsx

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,6 +1,15 @@
+import { Button } from './components/button';
+ 
 function App() {
   return (
-    <h1>Simbora para a Next Level Week!</h1>
+    <div>
+      <Button text="One Button" />
+      <Button text="Two Button" />
+      <Button text="Three Button" />
+      <Button text="Four Button" />
+      <Button />
+      <h1>Simbora para a Next Level Week!</h1>
+    </div>
   );
 }
 export default App;
