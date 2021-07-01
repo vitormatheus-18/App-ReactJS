8b6f4a5d7ae8f4840f8a67f6b1578218e7ac4e80
vitormatheus-18 <vitormmmorais@fac.pe.senac.br>
2021-06-30 21:54:05 -0300

    Alter scss for home

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -2,9 +2,11 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import '../styles/auth.scss';
+
 export function Home(){
     return (
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="Ilustração para perguntas e respostas"/>
                 <strong>Crie sala de Q&amp;A ao-vivo</strong>
