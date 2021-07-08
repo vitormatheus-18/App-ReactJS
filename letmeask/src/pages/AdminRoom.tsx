8cc67c3ee54be5dd6542533612c9af9c5f01f036
vitormatheus-18 <vitormmmorais@fac.pe.senac.br>
2021-07-08 18:34:50 -0300

    Create adminRoom page

--- letmeask/src/pages/AdminRoom.tsx
+++ letmeask/src/pages/AdminRoom.tsx
@@ -16,7 +16,7 @@ type RoomParams = {
     id: string;
 }
 
-export function Room(){
+export function AdminRoom(){
     const { user } = useAuth();
     const params = useParams<RoomParams>();
     const [newQuestion, setNewQuestion] = useState('');
