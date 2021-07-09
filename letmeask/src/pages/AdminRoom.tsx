eb69b8dc8fbd98f38a54caaf8a062efa4c579d7c
vitormatheus-18 <vitormmmorais@fac.pe.senac.br>
2021-07-08 21:33:21 -0300

    Create functionality of the closing room

--- letmeask/src/pages/AdminRoom.tsx
+++ letmeask/src/pages/AdminRoom.tsx
@@ -1,7 +1,8 @@
 import { type } from 'os';
 import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
 import { RoomCode } from '../components/RoomCode';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 // import { useAuth } from '../hooks/useAuth';
 import { useRoom } from '../hooks/useRoom'
@@ -18,43 +19,66 @@ type RoomParams = {
 
 export function AdminRoom(){
     // const { user } = useAuth();
+    const history = useHistory();
     const params = useParams<RoomParams>();
-
     const roomId = params.id;
 
 
     const { title, questions } = useRoom(roomId);
+
+    async function handleEndRoom() {
+        await database.ref(`rooms/${roomId}`).update({
+            endedAt: new Date(),
+        })
+
+        history.push('/');
+    }
+
+
+    async function handleDeleteQuestion(questionId: string) {
+        if (window.confirm('Tem certeza que deseja excluir está pergunta?')) {
+            await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+        }
+    }
     
     return(
-       <div id="page-room">
-           <header>
-               <div className="content">
+        <div id="page-room">
+            <header>
+                <div className="content">
                     <img src={logoImg} alt="letmeask"/>
                     <div>
                         <RoomCode code={ roomId } /> 
-                        <Button isOutlined>Encerrar sala</Button>
+                        <Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
                     </div>
-               </div>
-           </header>
+                </div>
+            </header>
 
-           <main>
-               <div className="room-title">
+            <main>
+                <div className="room-title">
                     <h1>Sala {title}</h1>
-                    { questions.length > 0 && <span>{questions.length} perguntas</span>}
-               </div>
-               <div className="question-list">             
-               {questions.map(question => {
-                   return (
-                    <Question
-                        key={question.id}
-                        content={question.content}
-                        author={question.author}
-                    />
-                   );
-               })}
-               </div>
-           </main>
-       </div>
+                    { questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+                </div>
+
+                <div className="question-list">             
+                    {questions.map(question => {
+                        return (
+                            <Question
+                                key={question.id}
+                                content={question.content}
+                                author={question.author}
+                            >
+                                <button
+                                    type="button"
+                                    onClick={() => handleDeleteQuestion(question.id)}
+                                >
+                                    <img src={deleteImg} alt="delete question" />
+                                </button>
+                            </Question>
+                        );
+                    })}
+                </div>
+            </main>
+        </div>
     );
     
 }
